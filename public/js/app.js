51543299c8a7b499029e5f4b4b348231e6864d34
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-02-02 14:21:43 +0000

    applied class to post page.  added 'change colour' button on page

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Add your JavaScript here.
+    const btn = document.getElementById('my-colour-button');
+    if (btn) {
+        btn.addEventListener('click', changeColours);
+    }
 });
+
+function changeColours(event) {
+    const posts = document.getElementsByClassName('post');
+
+    for (let post of posts) {
+        post.style.background = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    }
+}
+
+function buttonClick() {
+    console.log("Clicked");
+}
